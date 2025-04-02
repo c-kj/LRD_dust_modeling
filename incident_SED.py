7f99df5b32582f53d412e7223b14f97ad333ca89
陈可鉴 <128275399+c-kj@users.noreply.github.com>
2025-04-03 00:02:07 +0800

    🔨 chore(incident_SED): 修改 incident_SED.py 的内部处理逻辑，与李政融 paper 所用的代码保持一致，并添加更详细的注释。对外的接口不变。

--- incident_SED.py
+++ incident_SED.py
@@ -4,20 +4,26 @@ import astropy.constants as const
 
 from .utils import LogLogInterpolator
 
+#TODO 统一使用带单位的量，不要中途使用不带单位的数字
 class IncidentSED:
     def __init__(self, filename: str):
+        # the incident SED data need to be in the format of wavelength (Angstrom) and L_lambda (erg/s/Angstrom)
         #* 目前假定文件中的数据是有序且无重复的
         data = np.loadtxt(filename, comments='#', delimiter=' ', usecols=(0, 1))
         #* 目前把 wavelength 和 L_lambda 都反转，从而使得最终的 nu 递增，而 wavelength 对应地递减
         self.wavelength = data[::-1, 0] * u.AA  # 假定文件中的 wavelength 单位是 Angstrom
-        L_lambda = data[::-1, 1]
+        L_lambda = data[::-1, 1] * u.erg/u.s/u.AA
         
         self.nu = self.wavelength.to(u.Hz, u.spectral())  # 带单位 Hz
-        L_nu = L_lambda * self.wavelength.cgs.value**2 / const.c.cgs.value  # 未归一化
+        L_nu = L_lambda * self.wavelength**2 / const.c
+        L_nu = L_nu.to(u.erg/u.s/u.Hz).value
+
+        # 进行归一化，使得 bolometric luminosity 为 L_bol
+        # 2.0675e+15 是 1450 Angstrom 对应的频率
+        L_bol = 1e46     # erg/s
+        f_bol_UV = 4.4   #  从 bolometric 到 1450 Ang 处的 L_nu 的 correction 系数。 bolometric correction factor of f_{bol,UV} = 4.4 (Richards et al. 2006) 
+        NormalizedFactorAt1450 = L_bol/f_bol_UV/2.0675e+15 # erg/s/Hz
         
-        #TODO magic number 不太理解，有待修改
-        L_bol = 2.4e46 # erg/s
-        NormalizedFactorAt1450 = L_bol/4.4/2.0675e+15 # erg/s/Hz
-        
+        self.L_nu = L_nu / LogLogInterpolator(self.wavelength, L_nu)(1450 * u.AA) * NormalizedFactorAt1450  # 归一化后，L_nu 在 1450 Angstrom 处的值为 NormalizedFactorAt1450
         # 目前 L_nu 是不带单位的纯数值，单位应该是 erg/s/Hz
         self.L_nu = L_nu / LogLogInterpolator(self.wavelength, L_nu)(1450 * u.AA) * NormalizedFactorAt1450
