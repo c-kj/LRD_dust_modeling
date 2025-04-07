4875ffbff4de9a71d2b91708a5c53ab34a33a0f3
陈可鉴 <128275399+c-kj@users.noreply.github.com>
2025-04-07 20:53:36 +0800

    ✨ feat(utils,SED,opacity): 为基础设施添加单位支持
    - 在 utils 模块中为积分器、对数尺度积分器添加了单位支持
      - 把 get_value_unit 从类中移出来，并添加了 target_unit 参数用于指定单位
      - 添加了 @integrator_unit_support 装饰器，用于给函数积分器添加单位支持
      - 为 trapz_log、quad_vec_log 添加了单位支持
    
    - 为 SED 和 IncidentSED 类添加了单位支持
    - 为 OpacityData 类添加了单位支持
    
    - 以上所有改动，外部调用的 API 应当几乎不变
    
    - 新增 notebooks/加 units.ipynb 用于测试加单位的功能
    - 更新 TODOs

--- src/LRD_IR_spectrum/incident_SED.py
+++ src/LRD_IR_spectrum/incident_SED.py
@@ -7,11 +7,14 @@ from .utils import LogLogInterpolator
 
 #TODO 目前是一个简陋的实现，可以考虑改为 dataclass，并作为 IncidentSED 的基类？
 class SED:
-    def __init__(self, nu: Quantity, L_nu: Quantity):
+    @u.quantity_input
+    def __init__(self, nu: Quantity['frequency'], L_nu: Quantity[u.erg/u.s/u.Hz]):
         self.nu = nu
         self.L_nu = L_nu
 
 #TODO 统一使用带单位的量，不要中途使用不带单位的数字
+#TODO 可以考虑作为 SED 的子类 or 作为一个「另种构造方法」？
+#TODO 把 bolometric correction 作为可选项，参数可以传入
 class IncidentSED:
     def __init__(self, filename: str):
         # the incident SED data need to be in the format of wavelength (Angstrom) and L_lambda (erg/s/Angstrom)
@@ -21,18 +24,14 @@ class IncidentSED:
         self.wavelength = data[::-1, 0] * u.AA  # 假定文件中的 wavelength 单位是 Angstrom
         L_lambda = data[::-1, 1] * u.erg/u.s/u.AA
         
-        self.nu = self.wavelength.to(u.Hz, u.spectral())  # 带单位 Hz
+        self.nu: Quantity['frequency'] = self.wavelength.to(u.Hz, u.spectral())  # 带单位 Hz
         L_nu = L_lambda * self.wavelength**2 / const.c
-        L_nu = L_nu.to(u.erg/u.s/u.Hz).value
 
         # 进行归一化，使得 bolometric luminosity 为 L_bol
         # 2.0675e+15 是 1450 Angstrom 对应的频率
-        L_bol = 1e46     # erg/s
+        L_bol = 1e46 * u.erg/u.s     # erg/s
         f_bol_UV = 4.4   #  从 bolometric 到 1450 Ang 处的 L_nu 的 correction 系数。 bolometric correction factor of f_{bol,UV} = 4.4 (Richards et al. 2006) 
-        NormalizedFactorAt1450 = L_bol/f_bol_UV/2.0675e+15 # erg/s/Hz
+        nu_1450AA =  (1450 * u.AA).to(u.Hz, u.spectral())  # 1450 Angstrom 对应的频率
+        NormalizedFactorAt1450 = L_bol / f_bol_UV / nu_1450AA
         
-        self.L_nu = L_nu / LogLogInterpolator(self.wavelength, L_nu)(1450 * u.AA) * NormalizedFactorAt1450  # 归一化后，L_nu 在 1450 Angstrom 处的值为 NormalizedFactorAt1450
-        # 目前 L_nu 是不带单位的纯数值，单位应该是 erg/s/Hz
-        
-        #TEMP 把 L_nu 给带上单位
-        self.L_nu = self.L_nu * u.erg/u.s/u.Hz
+        self.L_nu: Quantity[u.erg/u.s/u.Hz] = L_nu / LogLogInterpolator(self.wavelength, L_nu)(1450 * u.AA) * NormalizedFactorAt1450  # 归一化后，L_nu 在 1450 Angstrom 处的值为 NormalizedFactorAt1450
--- src/LRD_IR_spectrum/utils.py
+++ src/LRD_IR_spectrum/utils.py
@@ -1,5 +1,7 @@
+from functools import wraps
+from typing import Callable
+
 import numpy as np
-import astropy.units as u
 from scipy import interpolate, integrate
 
 
@@ -15,18 +17,34 @@ except ImportError:
     INSTALLED_unyt = False
 
 
+def get_value_unit(x, target_unit=None):
+    """把一个量拆分成 value 和 unit。兼容 astropy、unyt 和 纯数字。
+    
+    target_unit 是一个单位，如果指定，则将 x 在该单位下拆分为 value 和 unit。
+    """
+    if INSTALLED_astropy and isinstance(x, u.Quantity):  # if astropy is installed, then enable the Quantity support
+        if target_unit is not None:
+            x <<= target_unit  # 原地转换到目标单位
+        return x.value, x.unit
+    
+    elif INSTALLED_unyt and isinstance(x, unyt.unyt_array):  # if unyt is installed, then enable the unyt_array support
+        if target_unit is not None:
+            x.convert_to_units(target_unit)  # 原地转换到目标单位
+        return x.value, x.units
+    
+    else:  # 纯数字。但目前没有处理 x 既不是 astropy 或 unyt 的量，也不是数字的情况。
+        if target_unit is not None:
+            raise ValueError("只有当 x 是 astropy 或 unyt 的量时，才能指定 target_unit。")
+        return x, 1
+
+def get_value(x):  # 目前为了简洁性，不接收 target_unit 参数
+    """如果 x 是 astropy 或 unyt 的量，则返回其 value，否则直接返回 x"""
+    return getattr(x, 'value', x)  # 利用了 astropy 和 unyt 的量都有 .value 属性，就不用分别处理了
+
 class ScaledInterpolator:
     @staticmethod
     def _identity(x):
         return x
-    
-    @staticmethod
-    def get_value_unit(x):
-        if INSTALLED_astropy and isinstance(x, u.Quantity):  # if astropy is installed, then enable the Quantity support
-            return x.value, x.unit
-        elif INSTALLED_unyt and isinstance(x, unyt.unyt_array):  # if unyt is installed, then enable the unyt_array support
-            return x.value, x.units
-        return x, 1
     
     def __init__(self, x, y, *, scale_x=(_identity, _identity), scale_y=(_identity, _identity), **kwargs):
         self.x = x
@@ -34,8 +52,8 @@ class ScaledInterpolator:
         self.f_x, self.inv_f_x = scale_x
         self.f_y, self.inv_f_y = scale_y
         
-        x_value, self.x_unit = self.get_value_unit(x)
-        y_value, self.y_unit = self.get_value_unit(y)
+        x_value, self.x_unit = get_value_unit(x)
+        y_value, self.y_unit = get_value_unit(y)
         
         #* 对于 kwargs 中的 fill_value，需要单独处理。如果指定为具体的值，要用 f_y 转换。
         if 'fill_value' in kwargs:
@@ -52,7 +70,7 @@ class ScaledInterpolator:
         self._interp = interpolate.interp1d(self.f_x(x_value), self.f_y(y_value), **kwargs)
         
     def __call__(self, x):
-        unit = self.get_value_unit(x)[1] 
+        unit = get_value_unit(x)[1] 
         if self.x_unit != 1 and unit != 1:   # if input x has a unit
             x = x.to_value(self.x_unit)   # convert to the same internal unit. Note the .to_value() method is the same in astropy and unyt
         elif self.x_unit == 1 and unit == 1:
@@ -82,17 +100,72 @@ class LogLinearInterpolator(ScaledInterpolator):
         super().__init__(x, y, scale_x=(np.log, np.exp), scale_y=(_identity, _identity), **kwargs)
         
         
+
+
+
+def integrator_unit_support(integrator: callable):
+    """装饰一个 用于函数积分 的函数 integrator(f,a,b, ...) -> (res, err, ...)，使其参数 f,a,b 和返回值都支持带单位。
+    integrator 例如 scipy.integrate 下的 quad, quad_vec 等
+    """
+    @wraps(integrator)
+    def wrapped_integrator(f, a, b, x_unit=None, *args, **kwargs):
+        """包装 integrator，使其参数 f,a,b 和返回值都支持带单位。
         
+        x_unit 用于指定积分时，自变量 x 应当在什么单位下分解为 value 和 unit （原则上不影响结果）。默认为 None 时，采用 a 的单位。
+        """
+        # 剥离 x 轴的值和单位。如果给定了 x_unit 则按照它来剥离，否则使用 a 的单位。
+        a_value, x_unit = get_value_unit(a, target_unit=x_unit)  # 至此，x_unit 不再可能是 None。要么是某个单位，要么是 1
+        b_value, b_unit = get_value_unit(b, target_unit=x_unit)  #* 必须保证 b 和 a 的单位是一致的！所以转换到 x_unit
+        
+        f_unit = get_value_unit(f(a))[1]  # 从 f(a) 中获取 f 的单位（假定 f 对于以 x_unit 的所有输入都给出同样的输出单位）
+        res_unit = f_unit * x_unit
+        
+        # 包装 f，使其接收和返回值都不带单位。不用区分 astropy 和 unyt，因为它们都有 .value 属性
+        if f_unit == 1:
+            integral_func = lambda x: f(x * x_unit)
+        else:
+            integral_func = lambda x: f(x * x_unit).value
+        
+        res, err, *rest = integrator(integral_func, a_value, b_value, *args, **kwargs) # 调用 integrator 执行积分
+
+        # 处理返回值：把 res 和 err 都附上单位
+        res *= res_unit
+        err *= res_unit
+        
+        return res, err, *rest
+    
+    return wrapped_integrator
+
+
+
 # 辅助函数，用于在自变量 log 尺度下做积分
+# \int f dx = \int f * x d(ln(x))
+# 适用于自变量天然地以 log 尺度分布的情况，普通的积分在 log(x) 轴的 sample 很不均匀
 
-def trapz_log(y, x, *args, **kwargs):
-    return integrate.trapezoid(y * x, np.log(x), *args, **kwargs)
+def trapz_log(y, x, *args, **kwargs):  # 目前只支持了 x 给定的情况。没有支持 x 不给出，依靠 dx 计算的情况。
+    """scipy 的 trapezoid 积分函数的变种，在 x 轴的 log 尺度下做积分。适用于 x 数组天然地以 log 尺度分布的情况。
 
+    y 和 x 都可以含有单位，支持 astropy 和 unyt。  
+    x 应当是正数。但目前不做检查（为了性能）。
+    """
+    
+    # scipy 的 trapezoid 函数本身就支持带单位输入。只是 np.log 不支持，所以只需要把 x 拆开即可。
+    # 又因为 log(x) 的差分与 x 的单位（或乘数）无关，所以只需要任意单位下 x 数组的值即可。单位由 y * x 决定。
+    x_value = get_value(x)
+    return integrate.trapezoid(y * x, np.log(x_value), *args, **kwargs)
+
+quad_vec_unit: Callable = integrator_unit_support(integrate.quad_vec)
+
+@integrator_unit_support
 def quad_vec_log(f, a, b, *args, **kwargs):
+    """scipy 的 quad_vec 积分函数的变种，在 x 轴的 log 尺度下做积分。  
+    适用于 x 变量天然地跨越多个数量级的情况。  
+    积分限 a, b 都应该是正数。
+    """
+    if a <= 0 or b <= 0:
+        raise ValueError("Both 'a' and 'b' must be positive for log-scale integration.")
+        
     def integrand(log_x):
         x = np.exp(log_x)
         return f(x) * x
     return integrate.quad_vec(integrand, np.log(a), np.log(b), *args, **kwargs)
-        
-        
-        
