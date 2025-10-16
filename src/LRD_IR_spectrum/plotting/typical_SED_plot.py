879fc2f29f326c4fd34bd1e16ff6d764ed27adee
陈可鉴 <128275399+c-kj@users.noreply.github.com>
2025-10-17 01:26:27 +0800

    ✨ feat(typical_SED_plot): Improve the TypicalSEDPlot
    - Refactor `plot_observed_table` function
        - adjustable marker properties
        - change the default non-detection marker style
    - Updated legend labels for clarity
    - Add optional `spec_table` parameter, for plotting spectra as background
    - Add A_V_high for showing the high A_V case (how bad it is)
    - Various plotting style improvements

--- src/LRD_IR_spectrum/plotting/typical_SED_plot.py
+++ src/LRD_IR_spectrum/plotting/typical_SED_plot.py
@@ -1,9 +1,10 @@
 # Fig.2: Typical SEDs with different parameters
 
-#* 由之前的纯过程式版本重构而来。渐进式重构，需要哪一部分灵活可变时，再拆出来单独调用。不需要改变的部分就写死在内部。
+# * 由之前的纯过程式版本重构而来。渐进式重构，需要哪一部分灵活可变时，再拆出来单独调用。不需要改变的部分就写死在内部。
 
 
 from functools import partial
+from typing import Any
 
 import numpy as np
 import astropy.units as u
@@ -12,6 +13,7 @@ from astropy.table import Table, QTable
 
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import Figure, Axes
+from matplotlib.artist import Artist
 from matplotlib.cm import ScalarMappable
 from matplotlib.lines import Line2D
 
@@ -24,45 +26,55 @@ from .plot_utils import add_wavelength_obs_xaxis, set_xylabel_with_unit
 
 np.seterr(over='ignore', divide='ignore')  # 忽略 overflow 和 divide by zero 的警告
 
+type detectionType = bool
 
-def plot_observed_table(ax: Axes, 
-                     *,
-                     x: str, 
-                     y: str, 
-                     table: QTable):
+
+def plot_observed_table(
+    ax: Axes,
+    *,
+    x: str,
+    y: str,
+    table: QTable,
+    observed_sed_line_label: str = 'Observed SED (A2744-45924)',
+    marker_props_dict: dict[detectionType, dict] = {},
+):
     """绘制观测数据表。分别画 detection 和 non-detection 的 marker 以及连起来的粗线
     
     table 要包含 x,y, 'detection' 列，table.meta 中要含有 'z' 信息
     """
     
-    scatter_handles = {}  # 收集 scatter 的 handles
-    
+    # marker 的默认属性，分别设置 detection 和 non-detection 的
+    marker_props_default_dict: dict[detectionType, dict[str, Any]] = {
+        True : dict(marker='o', color='r', edgecolor='k', lw=1, s=6**2,),
+        False: dict(marker='v', color='r', edgecolor='k', lw=1, s=6**2 * 1.3),
+    }
+
+    scatter_handles: dict[detectionType, Artist] = {}  # 收集 scatter 的 handles
+
     # 观测数据
     for is_detected in [True, False]:  # 分别绘制 detection 和 non-detection 的数据
         mask = table['detection'] == is_detected
         # 使用不同的 marker 属性
-        if is_detected:
-            marker_props = dict(marker='o', color='r', edgecolor='k', lw=1)
-        else:
-            marker_props = dict(marker='v', color='none', edgecolor='r', lw=1.5)
+        marker_props = marker_props_default_dict[is_detected]
+        marker_props |= marker_props_dict.get(is_detected, {})  # 覆盖默认属性
 
         _scatter_handle = ax.scatter(
             x=x, y=y, data=table[mask],
-            s=6**2,
+            # s=6**2,
             **marker_props,
             label={True: 'detection', False: 'non-detection'}[is_detected],
             zorder=5
         )
-        
+
         scatter_handles |= {is_detected: _scatter_handle}
-    
+
     # NIRCam 和 MIRI 的 detection 数据，连成粗线
     mask_sed_line = table['detection']
     observed_sed_line, = ax.plot(x, y, data=table[mask_sed_line],
             color='orange', alpha=.3, lw=6,
-            label='Observed SED (A2744-45924)'
+            label=observed_sed_line_label
             )
-    
+
     # 设定坐标轴的范围
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -74,7 +86,6 @@ def plot_observed_table(ax: Axes,
     return observed_sed_line, scatter_handles[True], scatter_handles[False]
 
 
-
 class TypicalSEDPlot:
 
     def __init__(
@@ -83,6 +94,7 @@ class TypicalSEDPlot:
         model_factory: Partial_A_V_Model,  # model_factory 的 opacity 和 observed_SED 参数会被覆盖
         opacity: OpacityData,
         SED_table: QTable,
+        spec_table: QTable | None = None,
         fmt = partial(quantity_to_latex, formatter='e'),  # 用于格式化 Quantity
         cmap=plt.get_cmap('viridis'),  # 使用默认的 colormap
         norm=plt.Normalize(vmin=0, vmax=1.15),  #TEMP 这里的归一化范围需要调整
@@ -92,6 +104,7 @@ class TypicalSEDPlot:
         
         self.opacity = opacity
         self.SED_table = SED_table
+        self.spec_table = spec_table
         
         self.fmt = fmt
         self.cmap = cmap
@@ -115,7 +128,7 @@ class TypicalSEDPlot:
         plt.rcParams['font.size'] = 14
         plt.rcParams['legend.fontsize'] = 'small'
         plt.rcParams['legend.title_fontsize'] = 'small'
-        plt.rcParams['legend.frameon'] = False
+        plt.rcParams['legend.edgecolor'] = 'none'
         
     
     def plot_background(self, ax: Axes):
@@ -124,8 +137,13 @@ class TypicalSEDPlot:
         
         ret = plot_observed_table(ax, x='wavelength_rest', y='nu_L_nu', table=table)
         
+        if self.spec_table is not None:
+            [self.handel_spec] = ax.step('wavelength_rest', 'nu_L_nu', data=self.spec_table, 
+                    label='JWST PRISM', 
+                    where='pre', color='k', alpha=.3, lw=1, zorder=-100)  # 放在最后面
+        
         ax.set_xlim(*[1e-1, 3e2] * u.um)
-        ax.set_ylim(*[1e43, 3e46] * u.erg/u.s)
+        ax.set_ylim(*[1e43, 1e47] * u.erg/u.s)
         add_wavelength_obs_xaxis(ax, z=table.meta['z'])  # 顶部的观测波长轴
         set_xylabel_with_unit(ax, xlabel=r"Rest-frame $\lambda$", ylabel=r"$\nu L_\nu$")  # 坐标轴 label
 
@@ -138,6 +156,7 @@ class TypicalSEDPlot:
         A_V_list: np.ndarray = np.arange(.2, 1.+1e-8, .2),
         n_0: Quantity,
         gamma: float,
+        A_V_high: float | None = None,  # 设定一个值，展示高 A_V 有多「糟糕」
     ):
         """Changing A_V. (n_0, gamma) 保持不变"""
         
@@ -150,21 +169,26 @@ class TypicalSEDPlot:
 
         handles_ax0_right = [] 
 
-        for A_V_val in A_V_list:
+        for A_V_val in A_V_list if A_V_high is None else np.append(A_V_list, A_V_high):  # 如果指定了 A_V_high，则也绘制它
             model = self.model_factory(A_V=A_V_val, n_0=n_0, gamma=gamma)  # 固定 (n_0, gamma)
             color = cmap(norm(A_V_val))
             
-            _line, = ax.loglog(model.opacity.wavelength, model.calc_nu_L_nu(), color=color, label=rf'{A_V_val:.1f} mag')  # IR re-emission
+            ax.plot(model.incident_SED.wavelength, model.incident_SED.nu_L_nu.to(u.erg/u.s), color=color, **props_incident)  # de-redden 入射光谱
+            
+            _line, = ax.loglog(model.opacity.wavelength, model.calc_nu_L_nu(), color=color, label=rf'{A_V_val:.1f} mag', **props_reprocessed)  # IR re-emission
+            if A_V_val == A_V_high:  # 把 A_V_high 的线单独设定样式
+                _line.set(alpha=1, ls=':')
+            
             handles_ax0_right.append(_line)
             
-            ax.plot(model.incident_SED.wavelength, model.incident_SED.nu_L_nu, color=color, **props_incident)  # de-redden 入射光谱
+            
 
         ax.set_title(rf"Changing $A_V$. $(\gamma, n_0)=$ ({model.gamma}, {fmt(model.n_0)})")  # 直接取最后一个模型的参数，反正它们都一样
 
         # 绘制背景并获取图例句柄
         _line_observed_sed, _handle_detection, _handle_nondetection = self.plot_background(ax)
         _handle_incident_sed = Line2D([], [], color='k', **props_incident, label='incident SED')
-        _handle_reprocessed_sed = Line2D([], [], color='k', **props_reprocessed, label='Reprocessed SED') 
+        _handle_reprocessed_sed = Line2D([], [], color='k', **props_reprocessed, label='re-emitted SED') 
 
         handles_ax0_left = [
             _line_observed_sed, 
@@ -173,16 +197,20 @@ class TypicalSEDPlot:
             _handle_incident_sed, 
             _handle_reprocessed_sed, 
         ]
+        
+        if self.spec_table is not None:
+            handles_ax0_left.insert(1, self.handel_spec)
 
         # 双图例
         bbox_to_anchor_y = 0.985
         legend_left = ax.legend(handles=handles_ax0_left, loc='upper left', bbox_to_anchor=(0.10, bbox_to_anchor_y)) 
         ax.add_artist(legend_left)   # 手动添加，从而能添加另一个图例
 
-        ax.legend(handles=handles_ax0_right[::-1], # 反序，这样图例的上下顺序与 SED 曲线一致
+        legend_right = ax.legend(handles=handles_ax0_right[::-1], # 反序，这样图例的上下顺序与 SED 曲线一致
                     loc='upper right', bbox_to_anchor=(0.98, bbox_to_anchor_y), title=r"$A_V$", )
+        ax.add_artist(legend_right)
         
-        return ax
+        return handles_ax0_left, handles_ax0_right
     
 
     def plot_panel_B(
@@ -225,7 +253,7 @@ class TypicalSEDPlot:
         self.plot_background(ax) 
 
         # 图例
-        ax.legend(handles=handles_ax1_right, loc='upper left', bbox_to_anchor=(0.02, bbox_to_anchor_y), title=r"$A_V$ (upper limit)", )
+        ax.legend(handles=handles_ax1_right, loc='upper left', bbox_to_anchor=(0.02, bbox_to_anchor_y), title=r"maximum allowed $A_V$", )
         
         return ax
     
@@ -251,22 +279,23 @@ class TypicalSEDPlot:
     def plot_figure(
         self, 
         *, 
+        A_V_list = np.arange(.2, 1.+1e-8, .2),  # panel A 中的 A_V 列表
         panel_A_paras = dict(n_0 = 1e3 * u.cm**-3, gamma = 0.5),
         paras_list: list[tuple[float, Quantity]],  # (gamma, n_0) 列表
+        A_V_high: float | None = None,  # 设定一个值，展示高 A_V 有多「糟糕」
         ):
         """组装各部分，从头绘制图 2."""
+        
+        self.set_style()
         
         #* 目前只是创建，但并不持有 fig, axs，因为暂时没有这个需求
         fig, axs = plt.subplots(ncols=2, figsize=(8*2, 6), layout='constrained',)
         axs: list[Axes]
         
-        self.set_style()
-        
-        self.plot_panel_A(ax=axs[0], A_V_list=np.arange(.2, 1.+1e-8, .2), **panel_A_paras)
+        self.plot_panel_A(ax=axs[0], A_V_list=A_V_list, **panel_A_paras, A_V_high=A_V_high)
         self.plot_panel_B(ax=axs[1], paras_list=paras_list)
         
         if False:  # 目前不启用 colorbar
             self.add_colorbar(fig, axs, label=r"$A_V$ [mag]", pad=0.01)
             
         return fig, axs
-        
